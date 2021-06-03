e6cf287f121d822751a537a4802718cec4a1f75b
zabroski <35234595+zabroski@users.noreply.github.com>
2021-06-02 21:46:21 -0400

    updating

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,18 +1,30 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHTTP from '../hooks/use-http';
+import { addQuote} from '../lib/api';
 
 const NewQuote = () => {
+  const {sendRequest, status } = useHTTP(addQuote)
     const history = useHistory();
+
+    useEffect(() => {
+      if(status === 'completed') {
+        history.push('/quotes');
+      }
+    }, [status, history]);
 
 
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData)
 
-    history.push('./quotes')
+    // history.push('./quotes')
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
+
+
