61c2b0d592e005b03f7e37d522ed9a56e52348cd
zabroski <35234595+zabroski@users.noreply.github.com>
2021-06-15 14:36:12 -0400

    app done for now

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -2,27 +2,25 @@ import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import QuoteForm from '../components/quotes/QuoteForm';
-import useHTTP from '../hooks/use-http';
-import { addQuote} from '../lib/api';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
-  const {sendRequest, status } = useHTTP(addQuote)
-    const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
 
-    useEffect(() => {
-      if(status === 'completed') {
-        history.push('/quotes');
-      }
-    }, [status, history]);
-
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [status, history]);
 
   const addQuoteHandler = (quoteData) => {
-    sendRequest(quoteData)
+    sendRequest(quoteData);
   };
 
   return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
-
 
